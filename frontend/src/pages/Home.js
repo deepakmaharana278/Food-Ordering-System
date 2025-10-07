4b0e9d941f0a047518da82d7291ac06bfeee7714
Jitendra Maharana <deepakmaharana3500@gmail.com>
2025-10-07 18:00:40 +0530

    home page info

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -78,6 +78,31 @@ const Home = () => {
         </div>
         </div>
       </section>
+
+      <section className="py-5 bg-dark text-white">
+        <div className="container text-center">
+          <h2>Ordering in 3 Simple Steps</h2>
+          <div className="row mt-4">
+            <div className="col-md-4">
+              <h4>1. Pick a dish you love</h4>
+              <p>Explore hundreds of mouth-watering options and choose what you creave!</p>
+            </div>
+            <div className="col-md-4">
+              <h4>2. Share your location</h4>
+              <p>Tell us where you are, and we'll handle the rest.</p>
+            </div>
+            <div className="col-md-4">
+              <h4>3. Enjoy doorstep delivery</h4>
+              <p>Relax while your meal arrives fast and fresh - pay when it's delivered!</p>
+            </div>
+          </div>
+          <p>Pay easily with Cash on Delivery - hassle-free!</p>
+        </div>
+      </section>
+      <section className="py-5 bg-warning text-center text-dark">
+        <h4>Ready to Satisfy Your Hunger?</h4>
+        <Link to='' className="btn btn-dark btn-lg">Browse full Menu</Link>
+      </section>
     </PublicLayout>
   );
 };
