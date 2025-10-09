164e2af42b8d7215c7cea8c22cbbe212b4ae0ac0
Jitendra Maharana <deepakmaharana3500@gmail.com>
2025-10-09 18:13:18 +0530

    Models for Address,Tracking,Payment,Review & Wishlist

--- backend/foodordering/models.py
+++ backend/foodordering/models.py
@@ -48,3 +48,71 @@ class Order(models.Model):
     # improve readable
     def __str__(self):
         return f"{self.order_number} ({self.user})"
+    
+# Order Address model
+class OrderAddress(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    order_number = models.CharField(max_length=100,null=True)
+    address = models.TextField()
+    order_time = models.DateTimeField(auto_now_add=True)
+    order_final_status = models.CharField(max_length=200,null=True)
+
+    # improve readable
+    def __str__(self):
+        return f"{self.order_number} ({self.user})"
+    
+# Tracking model
+class FoodTracking(models.Model):
+    order = models.ForeignKey(Order,on_delete=models.CASCADE)
+    remark = models.CharField(max_length=200,null=True)
+    status = models.CharField(max_length=200,null=True)
+    status_date = models.DateTimeField(auto_now_add=True)
+    order_cancelled_by_user = models.BooleanField(null=True)
+
+    # improve readable
+    def __str__(self):
+        return f"{self.order} - {self.status}"
+    
+# Payment Detail model
+class PaymentDetail(models.Model):
+    PAYMENT_CHOICE =[
+        ('cod','Cash on Delivery'),
+        ('online','Online Payment')
+    ]
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    order_number = models.CharField(max_length=100,null=True)
+    payment_mode = models.CharField(max_length=20,choices=PAYMENT_CHOICE)
+    card_number = models.CharField(max_length=20,null=True,blank=True)
+    expiry_date = models.CharField(max_length=10,null=True,blank=True)
+    cvv = models.CharField(max_length=5,null=True,blank=True)
+    payment_date = models.DateTimeField(auto_now_add=True)
+
+    # improve readable
+    def __str__(self):
+        return f"{self.order_number} - {self.payment_mode}"
+    
+    
+# Review model
+class Review(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    food = models.ForeignKey(Food,on_delete=models.CASCADE)
+    rating = models.PositiveIntegerField(default=1)
+    comment = models.TextField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # improve readable
+    def __str__(self):
+        return f"Review by {self.user.first_name} for {self.food.item_name} - {self.rating} stars"
+    
+# Wishlist model
+class Wishlist(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    food = models.ForeignKey(Food,on_delete=models.CASCADE)
+    added_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user','food')
+
+    # improve readable
+    def __str__(self):
+        return f"{self.user.first_name} {self.food.item_name}"
